d73e4a7eeec7c1dedd5cb167edeb7ab559421477
Vítor Carvalho <vitor.carvalho@ort.org.br>
2026-05-19 17:13:25 -0300

    mudadas coisas

--- App.js
+++ App.js
@@ -1,17 +1,53 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList, SectionList } from 'react-native';
 
 export default function App() {
+
   const produtos = [
     { 'id': 1, 'nome': 'Notebook' },
-    { 'id': 2, 'nome': 'Memoria'  },
-    { 'id': 3, 'nome': 'Monitor'  },
-    { 'id': 4, 'nome': 'Teclado'  },
+    { 'id': 2, 'nome': 'Memoria' },
+    { 'id': 3, 'nome': 'Monitor' },
+    { 'id': 4, 'nome': 'Teclado' },
+    { 'id': 5, 'nome': 'Mouse' },
   ]
 
+  const pratos = [
+    {
+      title: 'Prato Principal',
+      data: ['Pizza', 'Hamburguer', 'Risotto']
+    },
+    {
+      title: 'Acompanhamentos',
+      data: ['Batata frita', 'Cebola frita', 'Camarao']
+    },
+    {
+      title: 'Bebidas',
+      data: ['Agua', 'CocaCola', 'Vinho']
+    },
+  ]
 
   return (
     <View style={styles.container}>
+      <FlatList
+        data={produtos}
+        keyExtractor={item => item.id}
+        initialNumToRender={20}
+        renderItem={({ item }) => (
+          <Text>{item.id} - {item.nome}</Text>
+        )} />
 
+      <SectionList
+        sections={pratos}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+
+          <View>
+            <Text>{item}</Text>
+          </View>
+        )}
+
+        renderSectionHeader={({ section }) => (
+          <Text>{section.title}</Text>
+        )} />
     </View>
   );
 }
